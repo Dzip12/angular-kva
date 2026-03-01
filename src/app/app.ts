46dcb5f6034da5faf1b1964e6c043bec602444fa
Petar Jankovic <dule2299@gmail.com>
2026-03-01 23:26:56 +0100

    Add angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref],
+  imports: [RouterOutlet, RouterLinkWithHref, MatButtonModule, MatMenuModule, MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('kva-2026');
-  name = 'Petar Jankovic';
-  indeks = '2023200632';
+
 }
